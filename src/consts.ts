453bef6ba9565a9fa18ac00cebea63086fb67d7b
choco-luo <chocoyuru0081@gmail.com>
2026-02-22 11:21:11 +0800

    chore: 調整consts.ts的部分內容

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+// 這裡是全域資料的設定檔
+// 你可以把這裏面的檔案用 'import' 作為關鍵字導入進你想要的網頁頁面
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '海洋部落格 | 豆腐鯊';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,13 +3,16 @@ import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	// 載入 `src/content/blog/` 目錄中的 Markdown 和 MDX 檔案。
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
+	// 使用結構（schema）對 frontmatter 檢查型別是否正確
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
 			// Transform string to Date object
+			// 把字串 (string) 解析成 Date 物件
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
