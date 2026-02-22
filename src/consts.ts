8104f796211bd6845b5ac014afeb6286b7cfa4bb
choco-luo <chocoyuru0081@gmail.com>
2026-02-22 15:26:50 +0800

    chore: 調整consts.ts部分內容

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,5 @@
 // 這裡是全域資料的設定檔
 // 你可以把這裏面的檔案用 'import' 作為關鍵字導入進你想要的網頁頁面
 
-export const SITE_TITLE = '海洋部落格 | 豆腐鯊';
+export const SITE_TITLE = '海洋部落格 | 座頭鯨';
 export const SITE_DESCRIPTION = 'Welcome to my blog!';
